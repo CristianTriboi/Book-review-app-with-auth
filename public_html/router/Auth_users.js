c40ef9df8deed516880590ce1568889554af81e8
Cristian <51482096+CristianTriboi@users.noreply.github.com>
2025-02-17 15:58:17 +0100

    Create Auth_users.js

---
+++ public_html/router/Auth_users.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+const isValid = (username)=>{ 
+  
+}
+
+const authenticatedUser = (username,password)=>{
+  
+}
+
+regd_users.post("/login", (req,res) => {
+  
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;

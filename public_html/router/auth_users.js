6de595285373680fae53660437c9bf469d21ef7e
Cristian <51482096+CristianTriboi@users.noreply.github.com>
2025-03-20 12:01:54 +0100

    Implement the login route

--- public_html/router/auth_users.js
+++ public_html/router/auth_users.js
@@ -5,17 +5,41 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ 
-  
+const isValid = (username)=>{
+  let userMatches = users.filter((user)=>{
+    return user.username === username
+  });
+  return userMatches.length > 0;
 }
-
 const authenticatedUser = (username,password)=>{
-  
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  console.log(users)
+  return validusers.length > 0;
 }
 
+//only registered users can login
 regd_users.post("/login", (req,res) => {
-  
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
